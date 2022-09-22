ac4ec5db6e40abdd2d2f5e7c3a521257312eaa0f
edmarr2 <edmar_espinola@hotmail.com>
2022-09-22 11:03:55 -0400

    space between title and description

--- src/components/atoms/Title.vue
+++ src/components/atoms/Title.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <VueSparkles color="hsl(112.7, 77.2%, 39.6%)">
-      <h1 :style="{fontSize: '28px', marginTop: '15px'}">{{title}}</h1>
+      <h1 :style="{fontSize: '28px', marginTop: '30px'}">{{title}}</h1>
     </VueSparkles>
   </div>
 </template>
