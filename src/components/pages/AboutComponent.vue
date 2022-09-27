faf6b8a0508ef1dfc57f08144eea5086d6000caa
edmarr2 <edmar_espinola@hotmail.com>
2022-09-27 18:03:45 -0400

    add about

---
+++ src/components/pages/AboutComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <h2 class="title">
+        Sobre
+      </h2>
+      <div class="container">
+        <p style="margin-top: 20px;">
+          Plataforma desenvolvida na disciplina de Informática na Educação do curso de Sistemas de informação da <a href="https://portal.ufgd.edu.br/" class="link-ufgd">UFGD(Universidade Federal da Grande Dourados)</a>.
+        </p>
+        <p>Professora orientadora <a href="https://www.cieb.net.br/rede-ieb/pesquisador/1234" class="link-ufgd">Valguima Victoria Viana Aguiar Odakura</a></p> 
+        <h3>Alunos envolvidos:</h3>
+        <span class="names">
+          - Cleberson Teodoro;<br/>
+          - Davi Valcir Dos Santos Silva;<br/>
+          - Edmar Espinola;<br/>
+          - Gabriel Antonio Fim Morais;<br/>
+          - Wanderson Oliveira Pio;<br/>
+        </span>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+};
+</script>
+<style>
+.link-ufgd {
+  text-decoration: none;
+}
+.names {
+  text-align: end;
+
+}
+</style>
