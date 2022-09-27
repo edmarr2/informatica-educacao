84164c12e2f491df48a311f75ffef58eb9ffa2ea
V4LC1R <valcir212@gmail.com>
2022-09-26 20:49:24 -0400

    Ajuste no services de games/video

--- src/services/kids.js
+++ src/services/kids.js
@@ -13,13 +13,11 @@ const kids = [
       alt: 'Menina branca com cabelo marrom, com lenço amarelo na cabeça, camisa vermelha, saia azul, tenis vermelho segurando um smartphone',
       done: false,
       iframeUrl: [
-        {url: 'https://www.youtube.com/embed/IRYbq9EMyNM', title: 'Vídeo'},
+        {url: 'https://scratch.mit.edu/projects/733168337/embed', title: 'Vídeo'},
       ],
       games: [
-        {url: 'https://wordwall.net/pt/embed/e7eb10b24ccc4035bd87bbf42282938a?themeId=46&templateId=48&fontStackId=0', title: 'Jogo'},
-        {url: 'https://wordwall.net/pt/embed/e7eb10b24ccc4035bd87bbf42282938a?themeId=46&templateId=48&fontStackId=0', title: 'Jogo'},
-        {url: 'https://wordwall.net/pt/embed/e7eb10b24ccc4035bd87bbf42282938a?themeId=46&templateId=48&fontStackId=0', title: 'Jogo'},
-        {url: 'https://wordwall.net/pt/embed/e7eb10b24ccc4035bd87bbf42282938a?themeId=46&templateId=48&fontStackId=0', title: 'Jogo'},
+        {url: 'https://wordwall.net/pt/embed/70261737ebd44866b82ad1c4538246cf?themeId=46&templateId=48&fontStackId=0', title: 'Jogo'}
+        
       ],
       colorButton: '#6a4c93'
     },
@@ -32,7 +30,7 @@ const kids = [
       alt: 'Menina branca com cabelo preto, com capacete de metal e dois chifres brancos na cabeça, camisa marrom escuro, calça preta, botas marrom escura, segurando uma espada e escudo',
       done: false,
       iframeUrl: [
-        {url: 'https://www.youtube.com/embed/SoIpR-kbRcA', title: 'Vídeo'},
+        {url: 'https://scratch.mit.edu/projects/736661969/embed', title: 'Vídeo'},
       ],
       games: [
         {url: 'https://wordwall.net/pt/embed/c87e7039d8aa41e3bc504a5d45952a11?themeId=48&templateId=22&fontStackId=0', title: 'Jogo'},
@@ -48,10 +46,10 @@ const kids = [
       alt: 'Menino branco com cabelo preto, camisa amarelo limão, short azul, tenis azul segurando dinheiro',
       done: false,
       iframeUrl: [
-        {url: 'https://www.youtube.com/embed/IRYbq9EMyNM', title: 'Vídeo'},
+        {url: 'https://scratch.mit.edu/projects/733028265/embed', title: 'Vídeo'},
       ],
       games: [
-        {url: 'https://wordwall.net/pt/embed/e7eb10b24ccc4035bd87bbf42282938a?themeId=46&templateId=48&fontStackId=0', title: 'Jogo'},
+        {url: 'https://wordwall.net/pt/embed/0b76cc2779d545cf95582a058fa31298?themeId=23&templateId=49&fontStackId=0', title: 'Jogo'},
       ],
       colorButton: '#8ac926'
     },    
@@ -64,7 +62,7 @@ const kids = [
       alt: 'Menino branco com cabelo preto, camisa amarelo limão, short azul, tenis azul segurando dinheiro',
       done: false,
       iframeUrl: [
-        {url: 'https://www.youtube.com/embed/IRYbq9EMyNM', title: 'Vídeo'},
+        {url: 'https://scratch.mit.edu/projects/736429726/embed', title: 'Vídeo'},
       ],
       games: [
         {url: 'https://wordwall.net/pt/embed/e7eb10b24ccc4035bd87bbf42282938a?themeId=46&templateId=48&fontStackId=0', title: 'Jogo'},
