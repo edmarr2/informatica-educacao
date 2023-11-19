b991aa2e487b30a4cc8619e3761c9749e3e5288e
edmarr2 <edmar_espinola@hotmail.com>
2023-11-19 17:59:21 -0300

    unit test

---
+++ src/components/atoms/__tests__/title.spec.js
@@ -0,0 +1,26 @@
+import { mount } from '@vue/test-utils'
+import Title from '../Title.vue'
+
+describe('title component - Unit', () => {
+  it('should return instance vue', () => {
+    const wrapper = mount(Title,{
+      propsData: {
+        title: ''
+      }
+    })
+
+    expect(wrapper.vm).toBeDefined()
+  })
+  it('should update title component when prop value is changed', async () => {
+    const wrapper = mount(Title,{
+      propsData: {
+        title: ''
+      }
+    })
+
+    await wrapper.setProps({ title: 'teste' })
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.vm.title).toEqual('teste')
+  })
+})
